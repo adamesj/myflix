5ab62aa9247a1e6a46ecfa79f4aec4d910120461
Jimmy Adames <jimmy.adames@gmail.com>
2017-07-05 17:24:24 -0400

    Implemented specs for testing the search_by_title method in the video model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   match '/ui(/:action)', controller: 'ui', via: :get
+
   root 'videos#index'
+
   resources :videos
+
+  resources :videos, only: [:index] do
+    collection do
+      get 'search', to: 'videos#search'
+    end
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,76 +2,154 @@ require 'rails_helper'
 
 RSpec.describe Video, type: :model do
   it "saves itself" do
-    video = Video.new(
-      title: "Married with Children",
-      description: "Love and marriage! Love and marriage!",
+  park = Video.new(
+      title: "Parks and Recreation",
+      description: "You had me at Meat Tornado",
       small_cover_url: "small_image.jpg",
       large_cover_url: "large_image.jpg"
     )
-    video.save
-    expect(video.title).to eq "Married with Children"
+    park.save
+    expect(park.title).to eq "Parks and Recreation"
   end
 
   it "is valid with a title, description, and category" do
     category =  Category.create(name: "TV Comedies")
-    video = Video.create(
+    park = Video.create(
       title: "Married with Children",
-      description: "Love and marriage! Love and marriage!",
+      description: "You had me at Meat Tornado",
       small_cover_url: "small_image.jpg",
       large_cover_url: "large_image.jpg",
       category_id: category.id
     )
-    expect(video).to be_valid
+    expect(park).to be_valid
   end
 
   it { should belong_to(:category) }
 
   it "is invalid without a title" do
     category =  Category.create(name: "TV Comedies")
-    video = Video.new(
+    park = Video.new(
       title: nil,
-      description: "Love and marriage! Love and marriage!",
+      description: "You had me at Meat Tornado",
       small_cover_url: "small_image.jpg",
       large_cover_url: "large_image.jpg",
       category_id: category.id
     )
-    video.valid?
-    expect(video.errors[:title]).to include("can't be blank")
+    park.valid?
+    expect(park.errors[:title]).to include("can't be blank")
   end
 
 
   it "is invalid without a description" do
     category =  Category.create(name: "TV Comedies")
-    video = Video.new(
+    park = Video.new(
       title: "Married with Children",
       description: nil,
       small_cover_url: "small_image.jpg",
       large_cover_url: "large_image.jpg",
       category_id: category.id
     )
-    video.valid?
-    expect(video.errors[:description]).to include("can't be blank")
+    park.valid?
+    expect(park.errors[:description]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate title" do
     category =  Category.create(name: "TV Comedies")
     Video.create(
-      title: "Married with Children",
-      description: "Love and marriage! Love and marriage!",
+      title: "Parks and Recreation",
+      description: "You had me at Meat Tornado",
       small_cover_url: "small_image.jpg",
       large_cover_url: "large_image.jpg",
       category_id: category.id
     )
-    video = Video.new(
-      title: "Married with Children",
-      description: "Love and marriage! Love and marriage!",
+    park = Video.new(
+      title: "Parks and Recreation",
+      description: "You had me at Meat Tornado",
       small_cover_url: "small_image.jpg",
       large_cover_url: "large_image.jpg",
       category_id: category.id
     )
 
-    video.valid?
-    expect(video.errors[:title]).to include("has already been taken")
+    park.valid?
+    expect(park.errors[:title]).to include("has already been taken")
+  end
+
+  describe "search by title" do
+    before do
+      @category = Category.create(name: "TV Comedies")
+    end
+
+    it "returns an empty array if there is no match" do
+      simpsons = Video.create(
+        title: "The Simpsons",
+        description: "This long-running animated comedy focuses on an eponymous family.",
+        category_id: @category.id
+      )
+      futurama = Video.create(
+        title: "Futurama",
+        description: "Accidentally frozen, pizza-deliverer Fry wakes up 1,000 years in the future.",
+        category_id: @category.id
+      )
+      expect(Video.search_by_title("hello")).to eq [] # expects that there is no match
+    end
+
+    it "returns an array of one video for an exact match" do
+      simpsons = Video.create(
+        title: "The Simpsons",
+        description: "This long-running animated comedy focuses on an eponymous family.",
+        category_id: @category.id
+      )
+      futurama = Video.create(
+        title: "Futurama",
+        description: "Accidentally frozen, pizza-deliverer Fry wakes up 1,000 years in the future.",
+        category_id: @category.id
+      )
+      expect(Video.search_by_title("Futurama")).to eq [futurama]
+    end
+
+    it "returns an array of one video for a partial match" do
+      simpsons = Video.create(
+        title: "The Simpsons",
+        description: "This long-running animated comedy focuses on an eponymous family.",
+        category_id: @category.id
+      )
+      futurama = Video.create(
+        title: "Futurama",
+        description: "Accidentally frozen, pizza-deliverer Fry wakes up 1,000 years in the future.",
+        category_id: @category.id
+      )
+      expect(Video.search_by_title("urama")).to eq [futurama] # partial match
+    end
+
+    it "returns an array of all matches ordered by created_at" do
+      family_guy = Video.create(
+        title: "Family Guy",
+        description: "Sick, twisted and politically incorrect, the animated series features the adventures of the Griffin family.",
+        created_at: 1.week.ago,
+        category_id: @category.id
+      )
+      f_family = Video.create(
+        title: "F Is for Family",
+        description: "This animated raunchy comedy is inspired by the life of stand-up comic Bill Burr.",
+        created_at: 1.day.ago,
+        category_id: @category.id
+      )
+      expect(Video.search_by_title("Fam")).to eq [f_family, family_guy]
+    end
+
+    it "returns an empty array for a search with an empty string" do
+      simpsons = Video.create(
+        title: "The Simpsons",
+        description: "This long-running animated comedy focuses on an eponymous family.",
+        category_id: @category.id
+      )
+      futurama = Video.create(
+        title: "Futurama",
+        description: "Accidentally frozen, pizza-deliverer Fry wakes up 1,000 years in the future.",
+        category_id: @category.id
+      )
+      expect(Video.search_by_title("")).to eq []
+    end
   end
 
 end
