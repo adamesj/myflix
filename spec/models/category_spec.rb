82d5cc786612218316a8044524b543408261a09a
Jimmy Adames <jimmy.adames@gmail.com>
2017-07-05 13:46:32 -0400

    Implemented category model specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "saves itself" do
+    category = Category.new(
+      name: "TV Dramas"
+    )
+    category.save
+    expect(category.name).to eq "TV Dramas"
+  end
+
+  it "is valid with a name" do
+    category = Category.create(
+      name: "TV Dramas"
+    )
+    expect(category).to be_valid
+  end
+
+  it "is invalid without a name" do
+    category = Category.new(
+      name: nil
+    )
+    category.valid?
+    expect(category.errors[:name]).to include("can't be blank")
+  end
 end
